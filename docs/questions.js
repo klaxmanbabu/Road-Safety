a92129ea4f97dd7e30cefa1ffc793df4712a975d
klaxmanbabu <58721601+klaxmanbabu@users.noreply.github.com>
2026-03-10 12:05:33 +0530

    Create questions.js

---
+++ docs/questions.js
@@ -0,0 +1,234 @@
+window.QUESTION_BANK = [
+  {
+    id: 1,
+    q: "Select the Incorrect Statement with respect to Traffic Rules.",
+    options: [
+      "Always wear a seatbelt, even for short distances.",
+      "Stop at red lights and proceed only when the signal turns green.",
+      "You can overtake vehicles anytime you want on the road.",
+      "Use indicators to signal turns or lane changes."
+    ],
+    answerIndex: 2
+  },
+  {
+    id: 2,
+    q: "What is Tailgating?",
+    options: [
+      "Driving too closely behind another vehicle, leaving insufficient space to stop safely.",
+      "Parking your vehicle at a designated area during a sports event.",
+      "Maintaining a safe following distance to avoid collisions.",
+      "Overtaking another vehicle without using indicators."
+    ],
+    answerIndex: 0  
+  },
+  {
+    id: 4,
+    q: "What is the legal alcohol limit in India?",
+    options: [
+      "0.03% per 100ml of blood.",
+      "0.05% per 100ml of blood.",
+      "0.08% per 100ml of blood.",
+      "0.10% per 100ml of blood."
+    ],
+    answerIndex: 0
+  },
+  {
+    id: 5,
+    q: "What is the full form of PUCC?",
+    options: [
+      "Public Utility Control Certificate.",
+      "Public Urban Cleanliness Certificate.",
+      "Pollution Uncontrolled Certificate.",
+      "Pollution Under Control Certificate."
+    ],
+    answerIndex: 3
+  },
+  {
+    id: 6,
+    q: "Where footpaths are not provided, on which side should a pedestrian walk in India?",
+    options: [
+      "Left side of the road.",
+      "Right side of the road.",
+      "In the middle of the road.",
+      "Any side of the road."
+    ],
+    answerIndex: 1
+  },
+  {
+    id: 7,
+    q: "Always keep in mind, cross at __________, avoid sudden movements, and stay alert for vehicles that may not stop!",
+    options: [
+      "designated pedestrian crossings.",
+      "the middle of the road.",
+      "the fastest-moving lane.",
+      "anywhere convenient."
+    ],
+    answerIndex: 0
+  },
+  {
+    id: 8,
+    q: "When is it safe to cross at a zebra crossing?",
+    options: [
+      "When the traffic on one side has stopped.",
+      "When the traffic on both sides has slowed down.",
+      "When the traffic on both sides has stopped.",
+      "When the traffic on one side has slowed down."
+    ],
+    answerIndex: 2
+  },
+  {
+    id: 10,
+    q: "Which of the following is true regarding phone handling while driving?",
+    options: [
+      "Sending or reading messages while driving is acceptable if traffic is slow.",
+      "Using a hands-free option like Bluetooth is always safe while driving.",
+      "Using a phone while driving can cause dangerous distractions, but only in high-traffic conditions.",
+      "None of the above."
+    ],
+    answerIndex: 3
+  },
+  {
+    id: 11,
+    q: "When overtaking a vehicle in front, which of the following is the correct way to overtake?",
+    options: [
+      "Through the right side of that vehicle.",
+      "Through the left side of that vehicle.",
+      "Through the left side, if the road is wide.",
+      "Through any side, based on convenience."
+    ],
+    answerIndex: 0
+  },
+  {
+    id: 12,
+    q: "What should you do when you witness an accident in India?",
+    options: [
+      "Ignore the situation to avoid police questioning.",
+      "Help the victim because you are protected by the Good Samaritan Law.",
+      "Wait for someone else to help instead of getting involved.",
+      "Leave the scene immediately."
+    ],
+    answerIndex: 1
+  },
+  {
+    id: 13,
+    q: "Wearing a helmet while cycling on the main road is mandatory for both children and adults.",
+    options: ["True.", "False."],
+    answerIndex: 0
+  },
+  {
+    id: 14,
+    q: "Which is the most important criterion to consider while buying a helmet in India?",
+    options: ["Price.", "Style.", "ISI Certification.", "Comfort."],
+    answerIndex: 2
+  },
+  {
+    id: 15,
+    q: 'What does "Right of Way" mean in traffic rules?',
+    options: [
+      "The right to drive at any speed on a road.",
+      "The privilege of a vehicle or pedestrian to proceed first in a specific situation.",
+      "Permission to park anywhere on the road.",
+      "The ability to use a road without following traffic signals."
+    ],
+    answerIndex: 1
+  },
+  {
+    id: 16,
+    q: "At a T-intersection without traffic lights or a stop sign, where a driver is turning left or right from the terminating road into the continuing road, who has the right of way?",
+    options: [
+      "The driver turning left or right.",
+      "Any vehicle traveling on the continuing road.",
+      "Any pedestrian on the continuing road at or near the intersection.",
+      "Both pedestrians and vehicles on the continuing road."
+    ],
+    answerIndex: 3
+  },
+  {
+    id: 17,
+    q: "What should a driver do if they are driving and experience sudden heavy rainfall that reduces visibility?",
+    options: [
+      "Keep driving at the same speed and use hazard lights.",
+      "Slow down, maintain a safe distance, and use low-beam headlights.",
+      "Speed up to avoid getting stuck in traffic.",
+      "Turn off the vehicle’s headlights to avoid blinding other drivers."
+    ],
+    answerIndex: 1
+  },
+  {
+    id: 19,
+    q: "You are stuck in a road rage situation where the other person is fuming, but you're not at fault, what should you do?",
+    options: [
+      "Stay calm, avoid engaging, and contact the police if the situation escalates.",
+      "Engage in an argument to defend yourself.",
+      "Honk your horn to assert your presence and intimidate the other driver.",
+      "Drive off quickly to avoid further confrontation."
+    ],
+    answerIndex: 0
+  },
+  {
+    id: 20,
+    q: "Which of the following statements is true regarding lane usage?",
+    options: [
+      "The right lane, called the fast lane, is for slow-moving and heavy vehicles, while the left lane is for overtaking.",
+      "The left lane is for overtaking, the right lane is for slow-moving vehicles, and the middle lane should always be empty.",
+      "The right lane, called the fast lane, is for overtaking, the left lane is for slow-moving and heavy vehicles, and the middle lane is for vehicles moving at a moderate speed.",
+      "The middle lane should only be used by heavy vehicles, and the right lane is for both overtaking and slow vehicles."
+    ],
+    answerIndex: 2
+  },
+  {
+    id: 21,
+    q: "What should you do if your car breaks down in the middle of the highway?",
+    options: [
+      "Park the vehicle in the middle of the road and wait for help without any warnings.",
+      "Drive the vehicle to the nearest service station immediately, even if it is unsafe.",
+      "Exit the vehicle and walk to the nearest roadside help station without any precautions.",
+      "Park the vehicle on the shoulder of the road and switch on the hazard lights to warn the oncoming traffic."
+    ],
+    answerIndex: 3
+  },
+  {
+    id: 22,
+    q: "Which of the following is the most critical action to take when riding a two-wheeler on wet or slippery roads?",
+    options: [
+      "Accelerating smoothly to maintain momentum and reduce skidding.",
+      "Applying the front and rear brakes simultaneously with full force.",
+      "Reducing speed gradually, avoiding sudden turns, and increasing following distance.",
+      "Continuing to ride at the same speed, but being extra cautious of other vehicles."
+    ],
+    answerIndex: 2
+  },
+  {
+    id: 23,
+    q: "While driving through a main road, another vehicle is reversing from a side road what should you do?",
+    options: [
+      "Move to the opposite side of the road to avoid any potential collision.",
+      "Speed up and drive through quickly to avoid any delay.",
+      "Sound your horn and be prepared to stop, maintaining caution.",
+      "Keep driving at the same speed and assume the other vehicle will stop."
+    ],
+    answerIndex: 2
+  },
+  {
+    id: 24,
+    q: "When approaching and driving around a corner, what is the most important step to ensure safety?",
+    options: [
+      "Slow down before the corner, check for oncoming traffic, and steer smoothly.",
+      "Accelerate before the corner and immediately turn sharply.",
+      "Maintain the same speed, as the corner does not affect oncoming traffic.",
+      "Speed up just before the corner to make the turn faster."
+    ],
+    answerIndex: 0
+  },
+  {
+    id: 25,
+    q: "What should your attitude be towards road safety?",
+    options: [
+      "Experience matters most; following all rules is unnecessary for skilled drivers.",
+      "Road safety evolves constantly, requiring drivers to adapt to new rules, technologies, and habits for everyone's safety.",
+      "Road safety is mainly for inexperienced drivers; experienced drivers don’t need to change their habits.",
+      "Taking risks is fine as long as I’ve never been in an accident before."
+    ],
+    answerIndex: 1
+  }
+];
